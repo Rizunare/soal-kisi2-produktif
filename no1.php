e93315728d6f8bc620ada83576f4ab32cca305b5
Rizunare <tinynight05@gmail.com>
2023-03-12 18:08:36 +0700

    php

---
+++ no1.php
@@ -0,0 +1,13 @@
+<?php
+    $jamkerja = 9;
+        if($jamkerja < 7) {
+            echo"7jam bekerja mendapatkan gaji lembur";
+        } else if ($jamkerja = 9) {
+            echo "9jam bekerja mendapatkan gaji lembur";
+        } else if ($jamkerja > 8) {
+            echo "8jam bekerja mendapatkan gaji lembur";
+        } else if ($jamkerja > 5) {
+            echo "5jam bekerja mendapatkan gaji lembur";
+        } 
+    else echo "4jam bekerja mendapatkan gaji lembur"
+?>
---
+++ no2.php
@@ -0,0 +1,10 @@
+<?php
+    $i = 18;
+    if ($i > 20) {
+        echo "Vip=Kartu Atm, Buku Tabungan, Akses M-Bangking, Menjadi Prioritas";
+    } else if ($i == 18) {
+        echo "Kelas 1 = Kartu Atm, Akses M-Bangking, dan Buku Tabungan";
+    } else if ($i > 30) {
+        echo "Kelas 2 Kartu, Atm, dan Buku Tabungan";
+    }
+?>
